918842b13e628e4b9fa39c91f17cd50d975578d3
nick avlo <avlo@yahoo.com>
2023-08-11 12:47:42 -0700

    updated canary test class

--- src/main/java/com/prosilion/scdecisionmatrix/service/MessageService.java
+++ src/main/java/com/prosilion/scdecisionmatrix/service/MessageService.java
@@ -1,19 +1,11 @@
 package com.prosilion.scdecisionmatrix.service;
 
-import com.prosilion.scdecisionmatrix.service.security.AuthUserDetailsService;
-import com.prosilion.scdecisionmatrix.service.security.jdbc.AuthUserDetailServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class MessageService {
-
-	private final AuthUserDetailsService authUserDetailsService;
-
-	@Autowired
-	public MessageService(AuthUserDetailsService authUserDetailsService) {
-		this.authUserDetailsService = authUserDetailsService;
-	}
 
 	public String getSubscriptionMessage() {
 		return "TEST MESSAGE";
--- src/test/java/MessageServiceTest.java
+++ src/test/java/MessageServiceTest.java
@@ -1,18 +1,14 @@
 
 import com.prosilion.scdecisionmatrix.service.MessageService;
-import com.prosilion.scdecisionmatrix.service.security.jdbc.AuthUserDetailServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class MessageServiceTest {
 
-	@Mock
-	private AuthUserDetailServiceImpl mockAuthUserDetailService;
 	@InjectMocks
 	private MessageService messageService;
 
