bc1578e53783a5d20c6fb6c3398f479f359c4369
nick avlo <avlo@users.noreply.github.com>
2023-08-15 09:02:20 -0500

    Update AppUserAuthoritiesPopulator.java

--- src/main/java/com/prosilion/scdecisionmatrix/config/AppUserAuthoritiesPopulator.java
+++ src/main/java/com/prosilion/scdecisionmatrix/config/AppUserAuthoritiesPopulator.java
@@ -13,8 +13,7 @@ import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
 import org.springframework.stereotype.Component;
 
 /**
- * Application authorization/authorities bean serving in place of Ldap authorization/authorities
- * bean in favor of application specific authorization/authorities (user roles).
+ * Application authorization/authorities bean serving in place of Ldap authorization/authorities (roles).
  */
 @Component
 public class AppUserAuthoritiesPopulator implements LdapAuthoritiesPopulator {
